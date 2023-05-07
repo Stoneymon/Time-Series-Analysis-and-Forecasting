d26edc730b4317ade04017e13d26a5eaad2a285a
MelvinPariyadan <21imc10063@fh-krems.ac.at>
2023-05-07 11:21:30 +0200

    From last capstone
    Forgot to commit this part so doing it now

--- 37_sarima.R
+++ 37_sarima.R
@@ -1,34 +1,70 @@
-# Load the necessary libraries
-library(forecast)
+  # Load the necessary libraries
+  library(forecast)
+  
+  wallboxes_ts <- ts(wallboxes_jan_aug$total_power, start = c(2022, 1), frequency = 365)
 
-wallboxes_ts <- ts(wallboxes_jan_aug$total_power, start = c(2022, 1), frequency = 365)
+  ts_train <- window(wallboxes_ts, end = c(2022,186))
+  ts_test <- window(wallboxes_ts, start = c(2022, 187))
+  
+  sarima_model <- auto.arima(ts_train)
+  
+  checkresiduals(sarima_model)
+  
+  sarima_pred <- forecast(sarima_model, h = length(ts_test))
+  
+  accuracy(sarima_pred, ts_test)
+  
+  sarima_model <- auto.arima(ts_train)
+  
+  
+  sarima_pred <- forecast(sarima_model, h = 47)
+  
+  plot(sarima_pred, xlab = "Time", ylab = "Total Power", main = "SARIMA Forecast for Wallboxes Data")
+  lines(ts_test, col = "blue")
+  
+  
+  
+  ############################################################
+  # Cross Validation
 
-# Split the dataset into train and test sets (e.g., 80% train, 20% test)
-#train_index <- floor(length(wallboxes_ts) * 0.8)
-#test_index <- length(wallboxes_ts) - train_index
+  
+  #sarima_model <- auto.arima(ts_train)
+  
+  #sarima_pred <- forecast(sarima_model, h = length(ts_test))
+  #autoplot(sarima_pred)
 
-# Split the data into training and testing sets
-ts_train <- window(wallboxes_ts, start = c(2022, 1), end = c(2022,186))
-ts_test <- window(wallboxes_ts, start = c(2022, 187))
-
-# Fit the SARIMA model
-sarima_model <- auto.arima(ts_train)
-
-# Validate the SARIMA model
-checkresiduals(sarima_model)
-
-# Make predictions
-sarima_pred <- forecast(sarima_model, h = length(ts_test))
-
-# Evaluate the SARIMA model
-accuracy(sarima_pred, ts_test)
-
-# Fine-tune the SARIMA model
-sarima_model <- auto.arima(ts_train, stepwise = FALSE, approximation = FALSE)
-
-# Make final predictions
-sarima_pred <- forecast(sarima_model, h = 47)
-
-# Plot the results
-plot(sarima_pred, xlab = "Time", ylab = "Total Power", main = "SARIMA Forecast for Wallboxes Data")
-lines(ts_test, col = "blue")
+  #sarima_pred
+  
+  #predicted_vals <- sarima_pred$mean
+  #actual_vals <- ts_test
+  
+  
+  #actual_vals
+  
+  actual_vals <- c()
+  predicted_vals <- c()
+  
+  
+  for (i in 1:(length(wallboxes_ts)-186)) {
+    ts_train <- window(wallboxes_ts, end = c(2022, 186+i))
+    
+    sarima_model <- auto.arima(ts_train)
+    
+    sarima_pred <- forecast(sarima_model, h = 1)
+    
+    predicted_vals <- c(predicted_vals, sarima_pred$mean)
+    
+    actual_vals <- c(actual_vals, wallboxes_ts[186+i+1])
+  }
+  
+  checkresiduals(sarima_model)
+  
+  accuracy(predicted_vals, actual_vals)
+  
+  plot(predicted_vals, type = "l", xlab = "Days", ylab = "Total Power", main = "Time Series Cross-Validation SARIMA Predictions",ylim = c(0, 28000),col = "red")
+  lines(actual_vals, col = "blue")
+  legend("topright", legend = c("Predicted", "Actual"), col = c("red", "blue"), lty = 1)
+  
+ 
+  
+actual_vals
