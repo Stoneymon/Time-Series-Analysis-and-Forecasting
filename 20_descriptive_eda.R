f174927add5ca5af9591fcf51b714b62dc227340
MelvinPariyadan <21imc10063@fh-krems.ac.at>
2023-06-04 12:07:30 +0200

    minor fix

--- 20_descriptive_eda.R
+++ 20_descriptive_eda.R
@@ -27,20 +27,7 @@ plot_ly(wallboxes_jan_aug, x = ~Date, y = ~total_power, type = "scatter", mode="
   add_lines(x = ~Date, y = ~battery_SOC, mode = "lines", yaxis = "y2", name="Battery SOC", fill="none", line=list(color="rgb(255, 0, 0)")) %>%
   layout(yaxis2 = list(overlaying = "y", side = "right"))
 
-## total power per wallbox
 
-boxplot(total_power_per_wb[, 2:9])
-
-fig <- plot_ly(total_power_per_wb, x = ~Date, y = ~KEBA_1, name = 'Keba 1', type = 'scatter', mode = 'lines')
-fig <- fig %>% add_trace(y = ~KEBA_2, name = 'Keba 2', type = 'scatter', mode = 'lines')
-fig <- fig %>% add_trace(y = ~KEBA_3, name = 'Keba 3', type = 'scatter', mode = 'lines')
-fig <- fig %>% add_trace(y = ~Ladebox1, name = 'Ladebox 1', type = 'scatter', mode = 'lines')
-fig <- fig %>% add_trace(y = ~Ladebox2, name = 'Ladebox 2', type = 'scatter', mode = 'lines')
-fig <- fig %>% add_trace(y = ~Ladebox3, name = 'Ladebox 3', type = 'scatter', mode = 'lines')
-fig <- fig %>% add_trace(y = ~Delta, name = 'Delta', type = 'scatter', mode = 'lines')
-fig <- fig %>% add_trace(y = ~Raption_50, name = 'Raption 50', type = 'scatter', mode = 'lines')
-
-fig
 
 library(tibble)
 library(tidyr)
